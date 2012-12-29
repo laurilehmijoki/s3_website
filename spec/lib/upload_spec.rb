8afe04670e0c2ea9a4b106fccebe8077765d6d4c
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-12-29 17:08:10 +0200

    Add spec for the reduced redundancy param

--- spec/lib/upload_spec.rb
+++ spec/lib/upload_spec.rb
@@ -1,12 +1,38 @@
 require 'spec_helper'
 
 describe Jekyll::S3::Uploader do
+  describe 'reduced redundancy setting' do
+    it 'allows storing a file under the Reduced Redundancy Storage' do
+      file_to_upload = 'index.html'
+      s3_client = create_verifying_s3_client(file_to_upload) do |s3_object|
+        s3_object.should_receive(:write).with(
+          anything(),
+          :content_type => 'text/html',
+          :reduced_redundancy => true
+        )
+      end
+      Jekyll::S3::Uploader.send(:upload_file,
+                                file_to_upload,
+                                s3_client,
+                                'some_bucket_name',
+                                'features/support/test_site_dirs/my.blog.com/_site',
+                                s3_rrs = true)
+    end
+  end
+
   describe 'content type resolving' do
     it 'adds the content type of the uploaded CSS file into the S3 object' do
       file_to_upload = 'css/styles.css'
+      s3_client = create_verifying_s3_client(file_to_upload) do |s3_object|
+        s3_object.should_receive(:write).with(
+          anything(),
+          :content_type => 'text/css',
+          :reduced_redundancy => false
+        )
+      end
       Jekyll::S3::Uploader.send(:upload_file,
                                 file_to_upload,
-                                create_content_type_verifying_s3(file_to_upload),
+                                s3_client,
                                 'some_bucket_name',
                                 'features/support/test_site_dirs/my.blog.com/_site',
                                 s3_rrs = false)
@@ -14,41 +40,42 @@ describe Jekyll::S3::Uploader do
 
     it 'adds the content type of the uploaded HTML file into the S3 object' do
       file_to_upload = 'index.html'
+      s3_client = create_verifying_s3_client(file_to_upload) do |s3_object|
+        s3_object.should_receive(:write).with(
+          anything(),
+          :content_type => 'text/html',
+          :reduced_redundancy => false
+        )
+      end
       Jekyll::S3::Uploader.send(:upload_file,
                                 file_to_upload,
-                                create_content_type_verifying_s3(file_to_upload),
+                                s3_client,
                                 'some_bucket_name',
                                 'features/support/test_site_dirs/my.blog.com/_site',
                                 s3_rrs = false)
     end
+  end
 
-    def create_content_type_verifying_s3(file_to_upload)
-      def create_objects(file_to_upload)
-        def create_html_s3_object(file_to_upload)
-          s3_object = stub('s3_object')
-          content_type = (file_to_upload.end_with?'html') ? 'text/html' : 'text/css'
-          s3_object.should_receive(:write).with(
-            anything(),
-            # Ensure that the write method is called with the correct content type:
-            :content_type => content_type,
-            :reduced_redundancy => false
-          )
-          s3_object
-        end
-        objects = {}
-        objects[file_to_upload] = create_html_s3_object(file_to_upload)
-        objects
+  def create_verifying_s3_client(file_to_upload, &block)
+    def create_objects(file_to_upload, &block)
+      def create_html_s3_object(file_to_upload, &block)
+        s3_object = stub('s3_object')
+        yield s3_object
+        s3_object
       end
-      def create_bucket(file_to_upload)
-        bucket = stub('bucket')
-        bucket.stub(:objects => create_objects(file_to_upload))
-        bucket
-      end
-      buckets = stub('buckets')
-      buckets.stub(:[] => create_bucket(file_to_upload))
-      s3 = stub('s3')
-      s3.stub(:buckets => buckets)
-      s3
+      objects = {}
+      objects[file_to_upload] = create_html_s3_object(file_to_upload, &block)
+      objects
     end
+    def create_bucket(file_to_upload, &block)
+      bucket = stub('bucket')
+      bucket.stub(:objects => create_objects(file_to_upload, &block))
+      bucket
+    end
+    buckets = stub('buckets')
+    buckets.stub(:[] => create_bucket(file_to_upload, &block))
+    s3 = stub('s3')
+    s3.stub(:buckets => buckets)
+    s3
   end
 end
