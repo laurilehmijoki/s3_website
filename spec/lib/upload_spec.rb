4829d6b427b1b0a209975f6fecb090254e5379cd
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-12-31 16:10:03 +0200

    Add context info into uploader_spec.rb

--- spec/lib/upload_spec.rb
+++ spec/lib/upload_spec.rb
@@ -1,86 +1,88 @@
 require 'spec_helper'
 
 describe Jekyll::S3::Uploader do
-  describe 'reduced redundancy setting' do
-    let(:config) {
-      { 's3_reduced_redundancy' => true }
-    }
+  context '#upload_file' do
+    describe 'reduced redundancy setting' do
+      let(:config) {
+        { 's3_reduced_redundancy' => true }
+      }
 
-    it 'allows storing a file under the Reduced Redundancy Storage' do
-      file_to_upload = 'index.html'
-      s3_client = create_verifying_s3_client(file_to_upload) do |s3_object|
-        s3_object.should_receive(:write).with(
-          anything(),
-          :content_type => 'text/html',
-          :reduced_redundancy => true
-        )
+      it 'allows storing a file under the Reduced Redundancy Storage' do
+        file_to_upload = 'index.html'
+        s3_client = create_verifying_s3_client(file_to_upload) do |s3_object|
+          s3_object.should_receive(:write).with(
+            anything(),
+            :content_type => 'text/html',
+            :reduced_redundancy => true
+          )
+        end
+        Jekyll::S3::Uploader.send(:upload_file,
+                                  file_to_upload,
+                                  s3_client,
+                                  config,
+                                  'features/support/test_site_dirs/my.blog.com/_site')
       end
-      Jekyll::S3::Uploader.send(:upload_file,
-                                file_to_upload,
-                                s3_client,
-                                config,
-                                'features/support/test_site_dirs/my.blog.com/_site')
     end
-  end
 
-  describe 'content type resolving' do
-    let(:config) {
-      { 's3_reduced_redundancy' => false }
-    }
+    describe 'content type resolving' do
+      let(:config) {
+        { 's3_reduced_redundancy' => false }
+      }
 
-    it 'adds the content type of the uploaded CSS file into the S3 object' do
-      file_to_upload = 'css/styles.css'
-      s3_client = create_verifying_s3_client(file_to_upload) do |s3_object|
-        s3_object.should_receive(:write).with(
-          anything(),
-          :content_type => 'text/css',
-          :reduced_redundancy => false
-        )
+      it 'adds the content type of the uploaded CSS file into the S3 object' do
+        file_to_upload = 'css/styles.css'
+        s3_client = create_verifying_s3_client(file_to_upload) do |s3_object|
+          s3_object.should_receive(:write).with(
+            anything(),
+            :content_type => 'text/css',
+            :reduced_redundancy => false
+          )
+        end
+        Jekyll::S3::Uploader.send(:upload_file,
+                                  file_to_upload,
+                                  s3_client,
+                                  config,
+                                  'features/support/test_site_dirs/my.blog.com/_site')
       end
-      Jekyll::S3::Uploader.send(:upload_file,
-                                file_to_upload,
-                                s3_client,
-                                config,
-                                'features/support/test_site_dirs/my.blog.com/_site')
+
+      it 'adds the content type of the uploaded HTML file into the S3 object' do
+        file_to_upload = 'index.html'
+        s3_client = create_verifying_s3_client(file_to_upload) do |s3_object|
+          s3_object.should_receive(:write).with(
+            anything(),
+            :content_type => 'text/html',
+            :reduced_redundancy => false
+          )
+        end
+        Jekyll::S3::Uploader.send(:upload_file,
+                                  file_to_upload,
+                                  s3_client,
+                                  config,
+                                  'features/support/test_site_dirs/my.blog.com/_site')
+      end
     end
 
-    it 'adds the content type of the uploaded HTML file into the S3 object' do
-      file_to_upload = 'index.html'
-      s3_client = create_verifying_s3_client(file_to_upload) do |s3_object|
-        s3_object.should_receive(:write).with(
-          anything(),
-          :content_type => 'text/html',
-          :reduced_redundancy => false
-        )
+    def create_verifying_s3_client(file_to_upload, &block)
+      def create_objects(file_to_upload, &block)
+        def create_html_s3_object(file_to_upload, &block)
+          s3_object = stub('s3_object')
+          yield s3_object
+          s3_object
+        end
+        objects = {}
+        objects[file_to_upload] = create_html_s3_object(file_to_upload, &block)
+        objects
       end
-      Jekyll::S3::Uploader.send(:upload_file,
-                                file_to_upload,
-                                s3_client,
-                                config,
-                                'features/support/test_site_dirs/my.blog.com/_site')
-    end
-  end
-
-  def create_verifying_s3_client(file_to_upload, &block)
-    def create_objects(file_to_upload, &block)
-      def create_html_s3_object(file_to_upload, &block)
-        s3_object = stub('s3_object')
-        yield s3_object
-        s3_object
+      def create_bucket(file_to_upload, &block)
+        bucket = stub('bucket')
+        bucket.stub(:objects => create_objects(file_to_upload, &block))
+        bucket
       end
-      objects = {}
-      objects[file_to_upload] = create_html_s3_object(file_to_upload, &block)
-      objects
+      buckets = stub('buckets')
+      buckets.stub(:[] => create_bucket(file_to_upload, &block))
+      s3 = stub('s3')
+      s3.stub(:buckets => buckets)
+      s3
     end
-    def create_bucket(file_to_upload, &block)
-      bucket = stub('bucket')
-      bucket.stub(:objects => create_objects(file_to_upload, &block))
-      bucket
-    end
-    buckets = stub('buckets')
-    buckets.stub(:[] => create_bucket(file_to_upload, &block))
-    s3 = stub('s3')
-    s3.stub(:buckets => buckets)
-    s3
   end
 end
