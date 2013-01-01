c2ac9590c49c868bf551b299f4b2774e4adfac89
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-01-01 16:50:18 +0200

    Add examples of using Jekyll-s3 as a library

---
+++ features/jekyll-s3-as-library.feature
@@ -0,0 +1,23 @@
+Feature: Using Jekyll-s3 as a library
+
+  As a developer
+  I want to programmatically use the Jekyll-s3 API
+  So that I can extend my software with the capabilities of Jekyll-s3
+
+  @one-file-to-delete
+  Scenario: Developer wants feedback on how many files Jekyll-s3 deleted
+    When my Jekyll site is in "features/support/test_site_dirs/unpublish-a-post.com"
+    Then jekyll-s3 will push my blog to S3
+    And report that it deleted 1 file from S3
+
+  @new-and-changed-files
+  Scenario: Developer wants feedback on how many files Jekyll-s3 uploaded
+    When my Jekyll site is in "features/support/test_site_dirs/new-and-changed-files.com"
+    Then jekyll-s3 will push my blog to S3
+    And report that it uploaded 1 new and 1 changed files into S3
+
+  @s3-and-cloudfront-when-updating-a-file
+  Scenario: Developer wants feedback on how many Cloudfront items Jekyll-s3 invalidated
+    When my Jekyll site is in "features/support/test_site_dirs/cdn-powered.with-one-change.blog.fi"
+    Then jekyll-s3 will push my blog to S3 and invalidate the Cloudfront distribution
+    And report that it invalidated 2 Cloudfront item
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,3 +1,5 @@
+require 'rspec'
+
 When /^my Jekyll site is in "(.*?)"$/ do |blog_dir|
   @blog_dir = blog_dir
 end
@@ -15,10 +17,28 @@ Then /^the output should equal$/ do |expected_console_output|
   @console_output.should eq(expected_console_output)
 end
 
+Then /^report that it uploaded (\d+) new and (\d+) changed files into S3$/ do
+  |new_count, changed_count|
+  @amount_of_new_files.should == new_count.to_i
+  @amount_of_changed_files.should == changed_count.to_i
+end
+
+Then /^report that it invalidated (\d+) Cloudfront item$/ do |expected|
+  @amount_of_invalidated_items.should == expected.to_i
+end
+
+Then /^report that it deleted (\d+) file from S3$/ do |amount_of_deleted_files|
+  @amount_of_deleted_files.should == amount_of_deleted_files.to_i
+end
+
 def do_run
   @console_output = capture_stdout {
     in_headless_mode = true
-    Jekyll::S3::CLI.new.run("#{@blog_dir}/_site", in_headless_mode)
+    result = Jekyll::S3::CLI.new.run("#{@blog_dir}/_site", in_headless_mode)
+    @amount_of_new_files = result[:new_files_count]
+    @amount_of_changed_files = result[:changed_files_count]
+    @amount_of_deleted_files = result[:deleted_files_count]
+    @amount_of_invalidated_items = result[:invalidated_items_count]
   }
 end
 
