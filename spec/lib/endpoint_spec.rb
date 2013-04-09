bcd7fa1ba638af3f7426f5fa2a45282357c92e9d
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-04-09 16:45:01 +0300

    Add examples of Endpoint behaviour

--- spec/lib/endpoint_spec.rb
+++ spec/lib/endpoint_spec.rb
@@ -2,10 +2,20 @@ require 'spec_helper'
 require 'pp'
 
 describe Jekyll::S3::Endpoint do
-  
+
   it 'uses the "us-east-1" as the default location' do
     endpoint = Jekyll::S3::Endpoint.new
     endpoint.location_constraint.should eq(Jekyll::S3::Endpoint::DEFAULT_LOCATION_CONSTRAINT)
   end
 
+  it 'takes a valid location constraint as a constructor parameter' do
+    endpoint = Jekyll::S3::Endpoint.new('EU')
+    endpoint.location_constraint.should eq('EU')
+  end
+
+  it 'fails if the location constraint is invalid' do
+    expect {
+      Jekyll::S3::Endpoint.new('andromeda')
+    }.to raise_error
+  end
 end
