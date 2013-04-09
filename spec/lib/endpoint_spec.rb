094a7b4b35c9cbc3870047e072c910441f6a8b53
Alan deLevie <adelevie@gmail.com>
2013-04-09 12:48:07 -0400

    in endpoint_spec, separated defaulting logic from the specific value we default to

--- spec/lib/endpoint_spec.rb
+++ spec/lib/endpoint_spec.rb
@@ -3,9 +3,13 @@ require 'pp'
 
 describe Jekyll::S3::Endpoint do
 
-  it 'uses the "us-east-1" as the default location' do
+  it 'uses the DEFAULT_LOCATION_CONSTRAINT constant to set the default location constraint' do
     endpoint = Jekyll::S3::Endpoint.new
     endpoint.location_constraint.should eq(Jekyll::S3::Endpoint::DEFAULT_LOCATION_CONSTRAINT)
+  end
+
+  it 'uses the "us-east-1" as the default location' do
+    Jekyll::S3::Endpoint::DEFAULT_LOCATION_CONSTRAINT.should eq('us-east-1')
   end
 
   it 'takes a valid location constraint as a constructor parameter' do
@@ -19,3 +23,5 @@ describe Jekyll::S3::Endpoint do
     }.to raise_error
   end
 end
+
+
