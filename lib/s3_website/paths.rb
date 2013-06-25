d77e4cf2fb84620ea7e3f6fbb2cdd0e70e83c640
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-06-25 20:01:13 +0300

    Extract method @ paths.rb

--- lib/s3_website/paths.rb
+++ lib/s3_website/paths.rb
@@ -6,27 +6,37 @@ module S3Website
 
     def self.infer_site_path(candidate_path, pwd = Dir.pwd)
       if candidate_path == 'infer automatically'
-        site_path = site_paths.
-          map do |site_path|
-            pwd + '/' + site_path
-          end.
+        infer_automatically pwd
+      else
+        candidate_path_if_exists candidate_path
+      end
+    rescue NoWebsiteDirectoryFound => e
+      puts e.message
+      exit 1
+    end
+
+    private
+
+    def self.candidate_path_if_exists(candidate_path)
+      raise NoWebsiteDirectoryFound.new(
+        "Can't find a website in " + candidate_path
+      ) unless File.exists? candidate_path
+      candidate_path
+    end
+
+    def self.infer_automatically(pwd)
+      site_path = site_paths.
+        map do |site_path|
+        pwd + '/' + site_path
+        end.
           find do |site_path|
-            File.exists? site_path
+          File.exists? site_path
           end
         if site_path
           site_path
         else
           raise NoPredefinedWebsiteDirectoryFound
         end
-      else
-        raise NoWebsiteDirectoryFound.new(
-          "Can't find a website in " + candidate_path
-        ) unless File.exists? candidate_path
-        candidate_path
-      end
-    rescue NoWebsiteDirectoryFound => e
-      puts e.message
-      exit 1
     end
   end
 end
