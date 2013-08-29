0638e8453a15b612db3db143035cc145d3cf0b8c
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-08-29 05:06:20 +0300

    Reduce noice in invalidator_spec.rb

--- spec/lib/cloudfront/invalidator_spec.rb
+++ spec/lib/cloudfront/invalidator_spec.rb
@@ -2,21 +2,14 @@ require 'spec_helper'
 
 describe S3Website::Cloudfront::Invalidator do
   describe 'default behaviour' do
-    let(:config) {
-      {
-        's3_id' => 'aws id',
-        's3_secret' => 'aws secret',
-        'cloudfront_distribution_id' => 'EFXX'
-      }
-    }
+    let(:config) {{
+      's3_id' => 'aws id',
+      's3_secret' => 'aws secret',
+      'cloudfront_distribution_id' => 'EFXX'
+    }}
 
     it 'invalidates the root resource' do
-      invalidator = double('invalidator')
-      SimpleCloudfrontInvalidator::CloudfrontClient.
-        should_receive(:new).
-        with('aws id', 'aws secret', 'EFXX').
-        and_return(invalidator)
-
+      invalidator = create_simple_cloudfront_invalidator(config)
       invalidator.
         should_receive(:invalidate).
         with(['index.html', '']).
@@ -27,22 +20,15 @@ describe S3Website::Cloudfront::Invalidator do
   end
 
   describe 'option cloudfront_invalidate_root = true' do
-    let(:config) {
-      {
-        's3_id' => 'aws id',
-        's3_secret' => 'aws secret',
-        'cloudfront_distribution_id' => 'EFXX',
-        'cloudfront_invalidate_root' => true
-      }
-    }
+    let(:config) {{
+      's3_id' => 'aws id',
+      's3_secret' => 'aws secret',
+      'cloudfront_distribution_id' => 'EFXX',
+      'cloudfront_invalidate_root' => true
+    }}
 
     it 'invalidates all root resources' do
-      invalidator = double('invalidator')
-      SimpleCloudfrontInvalidator::CloudfrontClient.
-        should_receive(:new).
-        with('aws id', 'aws secret', 'EFXX').
-        and_return(invalidator)
-
+      invalidator = create_simple_cloudfront_invalidator(config)
       invalidator.
         should_receive(:invalidate).
         with(['article/', '']).
@@ -50,5 +36,14 @@ describe S3Website::Cloudfront::Invalidator do
 
       S3Website::Cloudfront::Invalidator.invalidate(config, ['article/index.html'])
     end
+  end
+
+  def create_simple_cloudfront_invalidator(config)
+    invalidator = double('invalidator')
+    SimpleCloudfrontInvalidator::CloudfrontClient.
+      should_receive(:new).
+      with(config['s3_id'], config['s3_secret'], config['cloudfront_distribution_id']).
+      and_return(invalidator)
+    invalidator
   end
 end
