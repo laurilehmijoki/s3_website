ec941e075ed57911dc4c329694e1d8e08fce72db
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-10-29 06:59:59 +0200

    Convert #mime_type to if-else

--- lib/s3_website/upload.rb
+++ lib/s3_website/upload.rb
@@ -113,8 +113,11 @@ module S3Website
     end
 
     def mime_type
-      return config['extensionless_mime_type'] if !!config['extensionless_mime_type'] && File.extname(path) == ""
-      MIME::Types.type_for(path).first
+      if !!config['extensionless_mime_type'] && File.extname(path) == ""
+        config['extensionless_mime_type']
+      else
+        MIME::Types.type_for(path).first
+      end
     end
   end
 end
