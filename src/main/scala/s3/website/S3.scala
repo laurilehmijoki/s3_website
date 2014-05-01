b18293e8f5bd6ebfdd9852faf5c47fcafe22b874
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2014-05-01 19:49:55 +0300

    Add Scala implementation for the push command

---
+++ src/main/scala/s3/website/S3.scala
@@ -0,0 +1,66 @@
+package s3.website
+
+import s3.website.model._
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.model.{ObjectMetadata, ListObjectsRequest, ObjectListing}
+import scala.collection.JavaConversions._
+import scala.util.Try
+import com.amazonaws.AmazonClientException
+import scala.util.Success
+import scala.util.Failure
+import scala.Some
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import s3.website.model.{UserError, IOError}
+
+object S3 {
+
+  case class SuccessfulUpload(s3Key: String)
+  case class FailedUpload(s3Key: String, error: Throwable)
+
+  def upload(uploadSource: UploadSource)(implicit site: Site, executor: ExecutionContextExecutor): Future[Either[FailedUpload, SuccessfulUpload]] =
+    Future {
+      val objectMetadata = {
+        val metaData = new ObjectMetadata()
+        uploadSource.contentEncoding.foreach(metaData.setContentEncoding)
+        metaData.setContentLength(uploadSource.contentLength)
+        metaData.setContentType(uploadSource.contentType)
+        metaData
+      }
+      s3Client.putObject(
+        site.config.s3_bucket, uploadSource.s3Key, uploadSource.openInputStream(), objectMetadata
+      )
+      Right(SuccessfulUpload(uploadSource.s3Key))
+    } recover {
+      case error => Left(FailedUpload(uploadSource.s3Key, error))
+    }
+
+  def resolveS3Files(implicit site: Site): Either[Error, Seq[S3File]] = Try {
+    objectSummaries()
+  } match {
+    case Success(remoteFiles) =>
+      Right(remoteFiles)
+    case Failure(error) if error.isInstanceOf[AmazonClientException] =>
+      Left(UserError(error.getMessage))
+    case Failure(error) =>
+      Left(IOError(error))
+  }
+
+  def objectSummaries(nextMarker: Option[String] = None)(implicit site: Site): Seq[S3File] = {
+    val objects: ObjectListing = s3Client.listObjects({
+      val req = new ListObjectsRequest()
+      req.setBucketName(site.config.s3_bucket)
+      nextMarker.foreach(req.setMarker)
+      req
+    })
+    val summaries = objects.getObjectSummaries map (S3File(_))
+    if (objects.isTruncated)
+      summaries ++ objectSummaries(Some(objects.getNextMarker))
+    else
+      summaries
+  }
+
+  def s3Client(implicit site: Site): AmazonS3Client =
+    new AmazonS3Client(new BasicAWSCredentials(site.config.s3_id, site.config.s3_secret))
+
+}
---
+++ src/main/scala/s3/website/model/Site.scala
@@ -0,0 +1,68 @@
+package s3.website.model
+
+import java.io.File
+import scala.util.{Failure, Success, Try}
+import org.yaml.snakeyaml.Yaml
+import s3.website.model.Config._
+import scala.io.Source.fromFile
+import scala.language.postfixOps
+
+case class Site(rootDirectory: String, config: Config) {
+  def localFilePath(file: File) = file.getAbsolutePath.replace(rootDirectory, "").replaceFirst("^/", "")
+}
+
+object Site {
+  def loadSite(yamlConfigPath: String, siteRootDirectory: String): Either[Error, Site] = {
+    val yamlObjectTry = for {
+      yamlString <- Try(fromFile(new File(yamlConfigPath)).mkString)
+      yamlWithErbEvaluated <- erbEval(yamlString)
+      yamlObject <- Try(new Yaml() load yamlWithErbEvaluated)
+    } yield yamlObject
+    yamlObjectTry match {
+      case Success(yamlObject) =>
+        implicit val unsafeYaml = UnsafeYaml(yamlObject)
+        val config: Either[Error, Config] = for {
+          s3_id <- loadRequiredString("s3_id").right
+          s3_secret <- loadRequiredString("s3_secret").right
+          s3_bucket <- loadRequiredString("s3_bucket").right
+          s3_endpoint <- loadEndpoint.right
+          max_age <- loadMaxAge.right
+          gzip <- loadOptionalBooleanOrStringSeq("gzip").right
+          gzip_zopfli <- loadOptionalBoolean("gzip_zopfli").right
+          extensionless_mime_type <- loadOptionalString("extensionless_mime_type").right
+          ignore_on_server <- loadOptionalStringOrStringSeq("ignore_on_server").right
+          exclude_from_upload <- loadOptionalStringOrStringSeq("exclude_from_upload").right
+          s3_reduced_redundancy <- loadOptionalBoolean("s3_reduced_redundancy").right
+          cloudfront_distribution_id <- loadOptionalString("cloudfront_distribution_id").right
+          cloudfront_invalidate_root <- loadOptionalBoolean("cloudfront_invalidate_root").right
+          concurrency_level <- loadOptionalInt("concurrency_level").right
+          redirects <- loadRedirects.right
+        } yield {
+          Config(
+            s3_id,
+            s3_secret,
+            s3_bucket,
+            s3_endpoint getOrElse S3Endpoint.defaultEndpoint,
+            max_age,
+            gzip,
+            gzip_zopfli,
+            extensionless_mime_type,
+            ignore_on_server = ignore_on_server,
+            exclude_from_upload = exclude_from_upload,
+            s3_reduced_redundancy,
+            cloudfront_distribution_id,
+            cloudfront_invalidate_root,
+            redirects,
+            concurrency_level.fold(20)(_ max 20) // At minimum, run 20 concurrent operations
+          )
+        }
+
+        config.right.map {
+          config =>
+            Site(siteRootDirectory, config)
+        }
+      case Failure(error) =>
+        Left(IOError(error))
+    }
+  }
+}
