9bbe7f6de96550975d30d64eb049034cb688c4d3
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2014-05-02 04:50:49 +0300

    Clarify push by isolating side effects

--- src/main/scala/s3/website/Push.scala
+++ src/main/scala/s3/website/Push.scala
@@ -1,57 +1,57 @@
 package s3.website
 
-import s3.website.model.{Site, Error, LocalFile}
 import s3.website.model.Site._
-import java.util.concurrent.Executors
-import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor}
+import scala.concurrent.{ExecutionContextExecutor, Future, Await}
 import scala.collection.parallel.ForkJoinTaskSupport
 import scala.concurrent.forkjoin.ForkJoinPool
 import scala.concurrent.duration._
-import scala.util.matching.Regex
 import com.lexicalscope.jewel.cli.CliFactory
 import scala.language.postfixOps
-import scala.util.Either.RightProjection
+import s3.website.Diff.resolveDiff
+import s3.website.S3.{resolveS3Files, upload}
+import scala.concurrent.ExecutionContext.fromExecutor
+import java.util.concurrent.Executors.newFixedThreadPool
+import s3.website.model.LocalFile.resolveLocalFiles
+import scala.collection.parallel.immutable.ParSeq
+import java.util.concurrent.ExecutorService
+import s3.website.model.Site
 
 object Push {
 
-  def pushSite(yamlConfigPath: String, siteRootDirectory: String) = {
-    val errorOrSite = loadSite(yamlConfigPath, siteRootDirectory)
-    errorOrSite.right.foreach( site =>
-      println(s"Deploying $siteRootDirectory/* to ${site.config.s3_bucket}")
-    )
-    val errorOrThreadPool = for {
-      loadedSite <- errorOrSite.right
-      s3Files    <- S3.resolveS3Files(loadedSite).right
-      localFiles <- LocalFile.resolveLocalFiles(loadedSite).right
+  def pushSite(implicit site: Site, executor: ExecutionContextExecutor): Int = {
+    println(s"Deploying ${site.rootDirectory}/* to ${site.config.s3_bucket}")
+
+    val errorsOrUploadReports = for {
+      s3Files    <- resolveS3Files.right
+      localFiles <- resolveLocalFiles.right
     } yield {
-      implicit val site = loadedSite
-      val threadPool = Executors.newFixedThreadPool(site.config.concurrency_level)
-      implicit val executor: ExecutionContextExecutor = ExecutionContext.fromExecutor(threadPool)
-      val uploadFutures = Diff
-        .resolveDiff(localFiles, s3Files)
-        .map(_.right.map(S3.upload))
-        .par
+      val uploadFutures = resolveDiff(localFiles, s3Files).map(_.right.map(upload)).par
       uploadFutures.tasksupport_=(new ForkJoinTaskSupport(new ForkJoinPool(site.config.concurrency_level)))
-      uploadFutures.map(_.right.foreach { uploadFuture =>
+
+      val uploadReports = uploadFutures.map(_.right.map { uploadFuture =>
         uploadFuture.map {
           case successOrFailure => successOrFailure match {
             case Right(success) => s"Successfully uploaded ${success.s3Key}"
             case Left(failure)  => s"Failed to upload ${failure.s3Key} (${failure.error.getMessage})"
           }
-        } onSuccess {
-          case report => println(report)
         }
-        Await.ready(uploadFuture, 1 day)
       })
-      threadPool
+      uploadReports
     }
-    errorOrThreadPool.right.foreach(_.shutdownNow())
-    errorOrThreadPool.left.foreach(error => println(error.message))
+    errorsOrUploadReports.right foreach { uploadReports => onUploadReports(uploadReports)}
+    errorsOrUploadReports.left foreach (err => println(s"Failed to push the site: ${err.message}"))
+    errorsOrUploadReports.fold(_ => 1, _ => 0)
   }
 
-  implicit class RegexContext(sc: StringContext) {
-    def r = new Regex(sc.parts.mkString, sc.parts.tail.map(_ => "x"): _*)
+  def onUploadReports(uploadReports: UploadReports)(implicit executor: ExecutionContextExecutor) {
+    uploadReports foreach (_.right.foreach {
+      rep => Await.ready(rep, 1 day)
+    })
+    uploadReports foreach (_.right.foreach(_.foreach(println)))
   }
+
+  type UploadReports = ParSeq[Either[model.Error, Future[String]]]
+  case class PushResult(threadPool: ExecutorService, uploadReports: UploadReports)
 
   trait CliArgs {
     import com.lexicalscope.jewel.cli.Option
@@ -64,9 +64,17 @@ object Push {
 
   def main(args: Array[String]) {
     val cliArgs = CliFactory.parseArguments(classOf[CliArgs], args:_*)
-    pushSite(
-      yamlConfigPath = cliArgs.configDir + "/s3_website.yml",
-      siteRootDirectory = cliArgs.site
-    )
+    val errorOrPushStatus = loadSite(cliArgs.configDir + "/s3_website.yml", cliArgs.site)
+      .right
+      .map {
+      implicit site =>
+        val threadPool = newFixedThreadPool(site.config.concurrency_level)
+        implicit val executor = fromExecutor(threadPool)
+        val pushStatus = pushSite
+        threadPool.shutdownNow()
+        pushStatus
+    }
+    errorOrPushStatus.left foreach (err => println(s"Could not load the site: ${err.message}"))
+    System.exit(errorOrPushStatus.fold(_ => 1, pushStatus => pushStatus))
   }
 }
