9534f08b747e6eb79ded2e01439ff0a671a011a6
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2014-05-02 09:11:31 +0300

    Add md5 tests

--- src/test/scala/s3/website/LocalFileSpec.scala
+++ src/test/scala/s3/website/LocalFileSpec.scala
@@ -8,19 +8,30 @@ import org.apache.commons.io.FileUtils.write
 import s3.website.model.Encoding.Gzip
 import org.apache.commons.io.IOUtils
 import java.util.zip.GZIPInputStream
+import org.apache.commons.codec.digest.DigestUtils
 
 class LocalFileSpec extends Specification {
 
   "#toUploadSource" should {
-    "gzip the file" in new TempFile(contents = "<html></html>") {
+    "gzip the file" in new TempFile(fileContents = "<html></html>") {
       val uploadSource = LocalFile.toUploadSource(LocalFile("index.html", tempFile, Some(Left(Gzip()))))
-      IOUtils.toString(new GZIPInputStream(uploadSource.right.get.openInputStream())) must equalTo("<html></html>")
+      IOUtils.toString(new GZIPInputStream(uploadSource.right.get.openInputStream())) must equalTo(fileContents)
+    }
+
+    "calculate the md5 for the gzipped data" in new TempFile(fileContents = "<html></html>") {
+      val uploadSource = LocalFile.toUploadSource(LocalFile("index.html", tempFile, Some(Left(Gzip()))))
+      uploadSource.right.get.md5 must equalTo("9b0474867213eeedf33dfe055680e7fa")
+    }
+
+    "calculate the md5 for the non-gzipped data" in new TempFile(fileContents = "<html></html>") {
+      val uploadSource = LocalFile.toUploadSource(LocalFile("index.html", tempFile, None))
+      uploadSource.right.get.md5 must equalTo(DigestUtils.md5Hex(fileContents))
     }
   }
 
-  class TempFile(contents: String) extends Scope {
+  class TempFile(val fileContents: String) extends Scope {
     val tempFile = createTempFile("test", "file")
     tempFile.deleteOnExit()
-    write(tempFile, contents)
+    write(tempFile, fileContents)
   }
 }
