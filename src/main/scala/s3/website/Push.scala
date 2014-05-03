771da1b7e1ba538c032e4489f0c76f07f1230ec6
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2014-05-03 17:28:29 +0300

    Support redirect

--- src/main/scala/s3/website/Push.scala
+++ src/main/scala/s3/website/Push.scala
@@ -8,25 +8,30 @@ import scala.concurrent.duration._
 import com.lexicalscope.jewel.cli.CliFactory
 import scala.language.postfixOps
 import s3.website.Diff.resolveDiff
-import s3.website.S3.{FailedUpload, SuccessfulUpload, resolveS3Files}
+import s3.website.S3.{S3ClientProvider, resolveS3Files, SuccessfulUpload, FailedUpload}
 import scala.concurrent.ExecutionContext.fromExecutor
 import java.util.concurrent.Executors.newFixedThreadPool
 import s3.website.model.LocalFile.resolveLocalFiles
 import scala.collection.parallel.ParSeq
 import java.util.concurrent.ExecutorService
-import s3.website.model.{Update, NewFile, Site}
+import s3.website.model._
 import s3.website.Implicits._
+import s3.website.model.Update
+import s3.website.model.NewFile
 
 object Push {
 
-  def pushSite(implicit site: Site, executor: ExecutionContextExecutor): Int = {
+  def pushSite(implicit site: Site, executor: ExecutionContextExecutor, s3ClientProvider: S3ClientProvider = S3.awsS3Client): Int = {
     println(s"Deploying ${site.rootDirectory}/* to ${site.config.s3_bucket}")
 
     val errorsOrUploadReports = for {
       s3Files    <- resolveS3Files.right
       localFiles <- resolveLocalFiles.right
     } yield {
-      val uploadReports = resolveDiff(localFiles, s3Files).map(_.right.map(new S3(S3.s3Client) upload)).par
+      val redirects = Redirect.resolveRedirects
+      val uploadReports = (redirects.toStream.map(Right(_)) ++ resolveDiff(localFiles, s3Files))
+        .map { errorOrUpload => errorOrUpload.right.map(new S3() upload ) }
+        .par
       uploadReports.tasksupport_=(new ForkJoinTaskSupport(new ForkJoinPool(site.config.concurrency_level)))
       uploadReports
     }
@@ -36,7 +41,8 @@ object Push {
     }
     errorsOrFinishedUploads.right.foreach { finishedUploads =>
       val pushCounts = pushCountsToString(resolvePushCounts(finishedUploads))
-      println(s"Done! $pushCounts Go visit: http://${site.config.s3_bucket}.${site.config.s3_endpoint.s3WebsiteHostname}")
+      println(s"$pushCounts")
+      println(s"Go visit: http://${site.config.s3_bucket}.${site.config.s3_endpoint.s3WebsiteHostname}")
     }
     errorsOrUploadReports.left foreach (err => println(s"Failed to push the site: ${err.message}"))
     errorsOrUploadReports.fold(
@@ -63,29 +69,32 @@ object Push {
       (error: model.Error) => counts.copy(failures = counts.failures + 1),
       failureOrSuccess => failureOrSuccess.fold(
         (failedUpload: FailedUpload) => counts.copy(failures = counts.failures + 1),
-        (success: SuccessfulUpload)  => success.upload.uploadType.fold(
-          (newFile: NewFile) => counts.copy(newFiles = counts.newFiles + 1),
-          (update: Update)   => counts.copy(updates = counts.updates + 1)
-        )
+        (success: SuccessfulUpload)  => success.upload.uploadType match {
+          case NewFile  => counts.copy(newFiles = counts.newFiles + 1)
+          case Update   => counts.copy(updates = counts.updates + 1)
+          case Redirect => counts.copy(redirects = counts.redirects + 1)
+        }
       )
     )
   }
 
   def pushCountsToString(pushCounts: PushCounts): String =
     pushCounts match {
-      case PushCounts(updates, newFiles, failures) if updates == 0 && newFiles == 0 && failures == 0 =>
+      case PushCounts(updates, newFiles, failures, redirects) if updates == 0 && newFiles == 0 && failures == 0 && redirects == 0 =>
         "There was nothing to push."
-      case PushCounts(updates, newFiles, failures) if updates > 0 && newFiles == 0 && failures == 0 =>
+      case PushCounts(updates, newFiles, failures, redirects) if updates > 0 && newFiles == 0 && failures == 0 && redirects == 0 =>
         s"Updated $updates file(s)."
-      case PushCounts(updates, newFiles, failures) if updates == 0 && newFiles >= 0 && failures == 0 =>
+      case PushCounts(updates, newFiles, failures, redirects) if updates == 0 && newFiles >= 0 && failures == 0 && redirects == 0 =>
         s"Created $newFiles file(s)."
-      case PushCounts(updates, newFiles, failures) if updates > 0 && newFiles > 0 && failures == 0 =>
+      case PushCounts(updates, newFiles, failures, redirects) if updates > 0 && newFiles > 0 && failures == 0 && redirects == 0 =>
         s"Created $newFiles and updated $updates file(s)."
-      case PushCounts(updates, newFiles, failures) =>
+      case PushCounts(updates, newFiles, failures, redirects) if failures == 0 =>
+        s"Created $newFiles and updated $updates file(s). Applied $redirects redirect(s)."
+      case PushCounts(updates, newFiles, failures, redirects) =>
         s"Created $newFiles and updated $updates file(s). $failures upload(s) failed!"
     }
 
-  case class PushCounts(updates: Int = 0, newFiles: Int = 0, failures: Int = 0/*, deletes: Int = -1 TODO implement delete*/)
+  case class PushCounts(updates: Int = 0, newFiles: Int = 0, failures: Int = 0, redirects: Int = 0/*, deletes: Int = -1 TODO implement delete*/)
   type FinishedUploads = ParSeq[Either[model.Error, Either[FailedUpload, SuccessfulUpload]]]
   type UploadReports = ParSeq[Either[model.Error, Future[Either[FailedUpload, SuccessfulUpload]]]]
   case class PushResult(threadPool: ExecutorService, uploadReports: UploadReports)
--- src/test/scala/s3/website/UploadSpec.scala
+++ src/test/scala/s3/website/UploadSpec.scala
@@ -15,54 +15,68 @@ import org.mockito.Mockito._
 import com.amazonaws.services.s3.AmazonS3
 import s3.website.model.Encoding.Gzip
 import scala.Some
-import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import s3.website.S3.S3ClientProvider
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import s3.website.model.Encoding.Gzip
+import s3.website.model.NewFile
+import scala.Some
 
 class UploadSpec extends Specification {
 
   "gzip" should {
     "gzip the file" in new TempFile {
-      val upload = LocalFile.toUpload(LocalFile("index.html", tempFile, Some(Left(Gzip()))))
-      IOUtils.toString(new GZIPInputStream(upload.right.get.openInputStream())) must equalTo(fileContents)
+      val upload = LocalFile.toUpload(LocalFile("index.html", tempFile, Some(Left(Gzip())))).right.get
+      IOUtils.toString(new GZIPInputStream(upload.essence.right.get.openInputStream())) must equalTo(fileContents)
     }
 
     "calculate the md5 for the gzipped data" in new TempFile {
-      val upload = LocalFile.toUpload(LocalFile("index.html", tempFile, Some(Left(Gzip()))))
-      upload.right.get.md5 must equalTo("9b0474867213eeedf33dfe055680e7fa")
+      val upload = LocalFile.toUpload(LocalFile("index.html", tempFile, Some(Left(Gzip())))).right.get
+      upload.essence.right.get.md5 must equalTo("9b0474867213eeedf33dfe055680e7fa")
     }
 
     "calculate the md5 for the non-gzipped data" in new TempFile {
-      val upload = LocalFile.toUpload(LocalFile("index.html", tempFile, None))
-      upload.right.get.md5 must equalTo(DigestUtils.md5Hex(fileContents))
+      val upload = LocalFile.toUpload(LocalFile("index.html", tempFile, None)).right.get
+      upload.essence.right.get.md5 must equalTo(DigestUtils.md5Hex(fileContents))
     }
   }
 
   "max-age in config" can {
     "be applied to all files" in new TempFile with MockS3 {
       val upload = LocalFile.toUpload(LocalFile("index.html", tempFile, None))(config.copy(max_age = Some(Left(60))))
-      asSeenByS3Client(upload.right.get).getCacheControl must equalTo("max-age=60")
+      asSeenByS3Client(upload.right.get).getMetadata.getCacheControl must equalTo("max-age=60")
     }
 
     "be applied to files that match the glob" in new TempFile with MockS3 {
       val upload = LocalFile.toUpload(LocalFile("index.html", tempFile, None))(config.copy(max_age = Some(Right(Map("*.html" -> 90)))))
-      asSeenByS3Client(upload.right.get).getCacheControl must equalTo("max-age=90")
+      asSeenByS3Client(upload.right.get).getMetadata.getCacheControl must equalTo("max-age=90")
     }
 
     "be applied to directories that match the glob" in new TempFile with MockS3 {
       val upload = LocalFile.toUpload(LocalFile("assets/lib/jquery.js", tempFile, None))(config.copy(max_age = Some(Right(Map("assets/**/*.js" -> 90)))))
-      asSeenByS3Client(upload.right.get).getCacheControl must equalTo("max-age=90")
+      asSeenByS3Client(upload.right.get).getMetadata.getCacheControl must equalTo("max-age=90")
     }
 
     "not be applied if the glob doesn't match" in new TempFile with MockS3 {
       val upload = LocalFile.toUpload(LocalFile("index.html", tempFile, None))(config.copy(max_age = Some(Right(Map("*.js" -> 90)))))
-      asSeenByS3Client(upload.right.get).getCacheControl must beNull
+      asSeenByS3Client(upload.right.get).getMetadata.getCacheControl must beNull
+    }
+  }
+
+  "redirect in config" should {
+    "result in a redirect instruction that is sent to AWS" in new SiteDirectory with MockS3 {
+      implicit val site = Site(siteDir.getAbsolutePath, defaultConfig.copy(redirects = Some(Map("index.php" -> "/index.html"))))
+      Push.pushSite
+      sentPutObjectRequest.getRedirectLocation must equalTo("/index.html")
     }
   }
 
   "#resolveLocalFiles" should {
-    "find .dot files" in new TestSite {
+    "find .dot files" in new TestSiteWithDotFile {
       val localFiles = LocalFile.resolveLocalFiles(site).right.get
       localFiles.map(_.s3Key) must contain(".vimrc")
     }
@@ -70,21 +84,37 @@ class UploadSpec extends Specification {
 
   trait MockS3 extends Scope {
     val amazonS3Client = mock(classOf[AmazonS3])
-    val s3 = new S3(_ => amazonS3Client)
-
-    def asSeenByS3Client(upload: Upload)(implicit config: Config): ObjectMetadata = {
-      Await.ready(s3.upload(upload withUploadType Left(NewFile())), Duration("1 s"))
-      val objectMetadataCaptor = ArgumentCaptor.forClass(classOf[ObjectMetadata])
-      verify(amazonS3Client).putObject(Matchers.anyObject(), Matchers.anyObject(), Matchers.anyObject(), objectMetadataCaptor.capture())
-      objectMetadataCaptor.getValue
+    implicit val s3ClientProvider: S3ClientProvider = _ => amazonS3Client
+    when(amazonS3Client.listObjects(Matchers.any(classOf[ListObjectsRequest]))).thenReturn {
+      val listing = new ObjectListing
+      //listing.getObjectSummaries.add(new ObjectSummary())
+      listing
     }
+    val s3 = new S3()
+
+    def asSeenByS3Client(upload: Upload)(implicit config: Config): PutObjectRequest = {
+      Await.ready(s3.upload(upload withUploadType NewFile), Duration("1 s"))
+      val req = ArgumentCaptor.forClass(classOf[PutObjectRequest])
+      verify(amazonS3Client).putObject(req.capture())
+      req.getValue
+    }
+
+    def sentPutObjectRequests: Seq[PutObjectRequest] = {
+      val req = ArgumentCaptor.forClass(classOf[PutObjectRequest])
+      verify(amazonS3Client).putObject(req.capture())
+      req.getAllValues
+    }
+
+    def sentPutObjectRequest = sentPutObjectRequests.ensuring(_.length == 1).head
   }
   
-  class TestSite extends Scope {
+  trait SiteDirectory extends Scope {
     val siteDir = new File(FileUtils.getTempDirectory, "site" + Random.nextLong())
     siteDir.mkdir()
     FileUtils.forceDeleteOnExit(siteDir)
-
+  }
+  
+  trait TestSiteWithDotFile extends Scope with SiteDirectory {
     val configYaml =
       """
         |s3_id: foo
@@ -106,22 +136,24 @@ class UploadSpec extends Specification {
     tempFile.deleteOnExit()
     write(tempFile, fileContents)
 
-    implicit val config: Config = Config(
-      s3_id = "foo",
-      s3_secret = "bar",
-      s3_bucket = "bucket",
-      s3_endpoint = S3Endpoint.defaultEndpoint,
-      max_age = None,
-      gzip = None,
-      gzip_zopfli = None,
-      extensionless_mime_type = None,
-      ignore_on_server = None,
-      exclude_from_upload = None,
-      s3_reduced_redundancy = None,
-      cloudfront_distribution_id = None,
-      cloudfront_invalidate_root = None,
-      redirects = None,
-      concurrency_level = 1
-    )
+    implicit val config: Config = defaultConfig
   }
+
+  val defaultConfig = Config(
+    s3_id = "foo",
+    s3_secret = "bar",
+    s3_bucket = "bucket",
+    s3_endpoint = S3Endpoint.defaultEndpoint,
+    max_age = None,
+    gzip = None,
+    gzip_zopfli = None,
+    extensionless_mime_type = None,
+    ignore_on_server = None,
+    exclude_from_upload = None,
+    s3_reduced_redundancy = None,
+    cloudfront_distribution_id = None,
+    cloudfront_invalidate_root = None,
+    redirects = None,
+    concurrency_level = 1
+  )
 }
