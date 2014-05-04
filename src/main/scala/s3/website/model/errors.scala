50db49abf2170fdd85b07d9d2578ba19fee5f685
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2014-05-04 19:20:07 +0300

    Remove UserException

--- src/main/scala/s3/website/model/errors.scala
+++ src/main/scala/s3/website/model/errors.scala
@@ -6,8 +6,6 @@ trait Error {
 
 case class UserError(message: String) extends Error
 
-class UserException(val causedBy: Throwable) extends RuntimeException
-
 case class IOError(exception: Throwable) extends Error {
   def message = exception.getMessage
 }
