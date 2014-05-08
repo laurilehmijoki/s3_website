45b24bfb315d3e41c5d4bc9a5991798ac33fa3dd
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2014-05-08 20:00:33 +0300

    Do not retry if AWS responds with 400 class of error

--- src/main/scala/s3/website/model/errors.scala
+++ src/main/scala/s3/website/model/errors.scala
@@ -1,13 +1,6 @@
 package s3.website.model
 
-import com.amazonaws.AmazonServiceException
-import com.amazonaws.AmazonServiceException.ErrorType.Client
 import s3.website.ErrorReport
-
-object Error {
-  def isClientError(error: Throwable) =
-    error.isInstanceOf[AmazonServiceException] && error.asInstanceOf[AmazonServiceException].getErrorType == Client
-}
 
 case class ClientError(reportMessage: String) extends ErrorReport
 
--- src/main/scala/s3/website/package.scala
+++ src/main/scala/s3/website/package.scala
@@ -1,11 +1,12 @@
 package s3
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
-import s3.website.model.Error._
 import s3.website.Logger._
 import scala.concurrent.duration.{TimeUnit, Duration}
 import s3.website.Utils._
 import s3.website.S3.{PushSuccessReport, PushFailureReport}
+import com.amazonaws.services.s3.model.AmazonS3Exception
+import com.amazonaws.AmazonServiceException
 
 package object website {
   trait Report {
@@ -29,7 +30,7 @@ package object website {
                            (createFailureReport: (Throwable) => L, retryAction: (Attempt) => Future[Either[L, R]])
                            (implicit retrySettings: RetrySettings, ec: ExecutionContextExecutor):
   PartialFunction[Throwable, Future[Either[L, R]]] = {
-    case error: Throwable if attempt == 6 || isClientError(error) =>
+    case error: Throwable if attempt == 6 || isIrrecoverable(error) =>
       val failureReport = createFailureReport(error)
       fail(failureReport.reportMessage)
       Future(Left(failureReport))
@@ -39,5 +40,14 @@ package object website {
       pending(s"${failureReport.reportMessage}. Trying again in $sleepDuration.")
       Thread.sleep(sleepDuration.toMillis)
       retryAction(attempt + 1)
+  }
+
+  def isIrrecoverable(error: Throwable) = {
+    val httpStatusCode =
+      error match {
+        case exception: AmazonServiceException => Some(exception.getStatusCode)
+        case _ => None
+      }
+    httpStatusCode.exists(c => c >= 400 && c < 500)
   }
 }
