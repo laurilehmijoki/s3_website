d1de9102aa982c457c7a89b89157abd928a94087
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2014-05-13 15:40:36 +0300

    Log file ignores

--- src/main/scala/s3/website/Diff.scala
+++ src/main/scala/s3/website/Diff.scala
@@ -6,13 +6,15 @@ import s3.website._
 
 object Diff {
 
-  def resolveDeletes(localFiles: Seq[LocalFile], s3Files: Seq[S3File], redirects: Seq[Upload with UploadTypeResolved])(implicit config: Config): Seq[S3File] = {
+  def resolveDeletes(localFiles: Seq[LocalFile], s3Files: Seq[S3File], redirects: Seq[Upload with UploadTypeResolved])
+                    (implicit config: Config, logger: Logger): Seq[S3File] = {
     val keysNotToBeDeleted: Set[String] = (localFiles ++ redirects).map(_.s3Key).toSet
     s3Files.filterNot { s3File =>
       val ignoreOnServer = config.ignore_on_server.exists(_.fold(
         (ignoreRegex: String)        => rubyRegexMatches(s3File.s3Key, ignoreRegex),
         (ignoreRegexes: Seq[String]) => ignoreRegexes.exists(rubyRegexMatches(s3File.s3Key, _))
       ))
+      if (ignoreOnServer) logger.debug(s"Ignoring ${s3File.s3Key} on server")
       keysNotToBeDeleted.exists(_ == s3File.s3Key) || ignoreOnServer
     }
   }
