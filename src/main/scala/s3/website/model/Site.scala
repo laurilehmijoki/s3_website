1073a74b535a59518ef0539fa121d00d10244300
Max Berger <max@berger.name>
2014-08-17 15:12:59 +0200

    Support windows by replacing File.separator with /

--- src/main/scala/s3/website/model/Site.scala
+++ src/main/scala/s3/website/model/Site.scala
@@ -15,7 +15,7 @@ import s3.website.model.Config.UnsafeYaml
 import scala.util.Success
 
 case class Site(rootDirectory: File, config: Config) {
-  def resolveS3Key(file: File) = file.getAbsolutePath.replace(rootDirectory.getAbsolutePath, "").replaceFirst("^/", "")
+  def resolveS3Key(file: File) = file.getAbsolutePath.replace(rootDirectory.getAbsolutePath, "").replace(File.separator,"/").replaceFirst("^/", "")
 
   def resolveFile(s3File: S3File): File = resolveFile(s3File.s3Key)
 
